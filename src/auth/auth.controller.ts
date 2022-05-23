9a9d8bb0b763868f02d6ea8eedeb51bc7e6ee40c
pedro3g <pgomesrodrigues801@gmail.com>
2022-05-22 23:11:28 -0300

    feat: create user endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import { sign } from 'jsonwebtoken';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() { name, email, password }: User) {
+    const errorFields: string[] = [];
+
+    if (await this.userService.findUnique({ email })) {
+      throw new BadRequestException({
+        status: 'error.userAlreadyExists',
+      });
+    }
+
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email,
+      )
+    ) {
+      errorFields.push('email');
+    }
+    if (name.length < 3 || name.length > 255) {
+      errorFields.push('name');
+    }
+    if (password.length < 6 || password.length > 255) {
+      errorFields.push('password');
+    }
+
+    if (!!errorFields.length) {
+      throw new BadRequestException({
+        status: 'error.invalidField',
+        fields: errorFields.sort(),
+      });
+    }
+
+    const user = await this.userService.create({ name, email, password });
+
+    return {
+      token: sign(
+        {
+          id: user.id,
+          emal: user.email,
+        },
+        process.env.JWT_KEY,
+      ),
+      user: { id: user.id, name: user.name, email: user.email },
+    };
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
 import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController],
+  providers: [UserService, PrismaService],
 })
 export class AuthModule {}
