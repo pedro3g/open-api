85c0b87d168773552b944cafaa62caf9c8b0fc63
pedro3g <pgomesrodrigues801@gmail.com>
2022-05-22 21:50:42 -0300

    test: validate user password

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from './user.service';
+import { hashSync } from 'bcrypt';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -44,5 +45,29 @@ describe('UserService', () => {
     prismaService.user.create = jest.fn().mockReturnValueOnce(mockUser);
 
     expect(await userService.create(mockUser)).toEqual(mockUser);
+  });
+
+  it('validate user password', async () => {
+    const mockUser: User = {
+      id: 'abc-123',
+      email: 'user@email.com',
+      name: 'User',
+      password: hashSync('user123', 10),
+    };
+
+    prismaService.user.findUnique = jest.fn().mockReturnValueOnce(mockUser);
+
+    expect(
+      await userService.validateUser({
+        email: mockUser.email,
+        password: 'user123',
+      }),
+    ).toEqual(mockUser);
+    expect(
+      await userService.validateUser({
+        email: mockUser.email,
+        password: 'user12',
+      }),
+    ).toEqual(null);
   });
 });
